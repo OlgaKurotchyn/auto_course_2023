390060b3f57e85bcb1bf6a5b9b8a572ac0e5f694
Olga Kn <olha.kurotchyn@globallogic.com>
2023-04-17 18:39:05 +0300

    Improved config module

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,3 +1,5 @@
+from src.config.config import CONFIG
+
 
 def test_search_repo ():
     assert 1 == 1
@@ -7,15 +9,17 @@ def test_user_age_is_42(user):
     assert user.age == 43
 
 
+def test_user_age_is_50(user):
+    assert user.age == 50
 
-def test_user_age_is_50():
-    print("Create user")
-    user = User(42)
 
-    assert user.age == 43
+def test_http_request():
+    print(CONFIG.get("BASE_URL_API"))
 
-    print("Remove user")
-    user.remove()
+
+def test_ui_POM():
+    print(CONFIG.get("BASE_URL_UI"))
+    
 
 
 
